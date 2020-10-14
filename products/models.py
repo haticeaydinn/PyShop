ae6c64f8786a9170d474baff6790903181e5ce4e
Hatice Aydın <hatice3178@yahoo.com.tr>
2020-10-14 16:39:03 +0300

    added Product, Offer tables and made migration

--- products/models.py
+++ products/models.py
@@ -6,7 +6,10 @@ class Products(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     stock = models.IntegerField()
-    image_url = models.CharField(msx_length=2083)
+    image_url = models.CharField(max_length=2083)
 
 
-
+class Offer(models.Model):
+    code = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
