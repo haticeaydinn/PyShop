335b89187e1111e2715f4c3f8aaa2b9550766e6f
Hatice Aydın <hatice3178@yahoo.com.tr>
2020-10-15 03:03:49 +0300

    create template for product page

--- products/views.py
+++ products/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 
 # Create your views here.
 # index means main home page
 def index(request):
-    return HttpResponse('Welcome to Products Page')
-
+    products = Product.objects.all()  # to get all products in table
+    # Product.object.filter() to filter ex. cheaper than 2 dollars
+    # Product.object.get() to get one product
+    # Product.object.save()
+    # return HttpResponse('Welcome to Products Page')
+    return render(request, 'index.html', {'products': products})
 
 def new(request):
     return HttpResponse('New Products')
